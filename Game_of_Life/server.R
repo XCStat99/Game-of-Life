483bb7dc9e78ca4581941c9c28643f37def9dfe9
XCStat99 <alex.mcintosh1000@gmail.com>
2024-02-02 22:02:58 +0000

    Extra life parameters added

--- Game_of_Life/server.R
+++ Game_of_Life/server.R
@@ -14,7 +14,7 @@ library(tidyverse)
 library(plotly)
 library(htmlwidgets)
 
-# Define server logic required to draw a histogram
+
 function(input, output, session) {
 
     n <- eventReactive(input$run, {
@@ -26,14 +26,26 @@ function(input, output, session) {
     prob_life <-  eventReactive(input$run, {
         input$prob_life
     })
+    B_R <-  eventReactive(input$run, {
+        input$B_R
+    })
+    D_U <-  eventReactive(input$run, {
+        input$D_U
+    })
+    D_O <-  eventReactive(input$run, {
+        input$D_O
+    })
     observe({
         #reassign variables to save on typing
         #define 'habitat' size
+        #number of rows given by n
         n <- n()
+        #number of columns is a fixed ratio of the rows, this makes plotting easier
+        m <- n()
         #define evolution iterations
         frame_tot <- frame_tot()
         #initate starting conditions in a matrix, 
-        M<-matrix(rbinom(n^2,1,prob_life()),nrow=n)
+        M<-matrix(rbinom(n*m,1,prob_life()),nrow=n)
         
         #Alive and dead cells are assigned 1 or 0:
         #Alive = 1
@@ -46,14 +58,14 @@ function(input, output, session) {
         
         for (i in 1:frame_tot){
             # make shifted copies of the original M matrix
-            M1 = cbind( rep(0,n) , M[,-n] )
-            M2 = rbind(rep(0,n),cbind(rep(0,n-1),M[-n,-n]))
-            M3 = rbind(rep(0,n),M[-n,])
-            M4 = rbind(rep(0,n),cbind(M[-n,-1],rep(0,n-1)))
+            M1 = cbind( rep(0,n) , M[,-m] )
+            M2 = rbind(rep(0,m),cbind(rep(0,n-1),M[-n,-m]))
+            M3 = rbind(rep(0,m),M[-n,])
+            M4 = rbind(rep(0,m),cbind(M[-n,-1],rep(0,n-1)))
             M5 = cbind(M[,-1],rep(0,n))
-            M6 = rbind(cbind(M[-1,-1],rep(0,n-1)),rep(0,n))
-            M7 = rbind(M[-1,],rep(0,n))
-            M8 = rbind(cbind(rep(0,n-1),M[-1,-n]),rep(0,n))
+            M6 = rbind(cbind(M[-1,-1],rep(0,n-1)),rep(0,m))
+            M7 = rbind(M[-1,],rep(0,m))
+            M8 = rbind(cbind(rep(0,n-1),M[-1,-n]),rep(0,m))
             
             # summation of the shifted matrices
             M_tot <- M1 + M2 + M3 + M4 + M5 + M6 + M7 + M8
@@ -61,16 +73,16 @@ function(input, output, session) {
             # apply the rules of Conway's Game of Life
             M_temp <- M
             # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-            M_temp[M==0 & M_tot==3] <- 1
+            M_temp[M==0 & M_tot==B_R()] <- 1
             # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-            M_temp[M==1 & M_tot<2] <- 0
+            M_temp[M==1 & M_tot<D_U()] <- 0
             # Any live cell with more than three live neighbours dies, as if by overpopulation.
-            M_temp[M==1 & M_tot>3] <- 0
+            M_temp[M==1 & M_tot>D_O()] <- 0
             #populate 3D matrix for animation
             M_all[,,i] <- M_temp
             M <- M_temp
             #check relative population  and store to vector
-            rel_pop <- sum(M_temp)*100/(n^2)
+            rel_pop <- sum(M_temp)*100/(n*m)
             M_pop[nrow(M_pop)+1,] <- c(rel_pop,i)
             #check if mass extinction has occured
             if(rel_pop ==0){
@@ -90,34 +102,87 @@ function(input, output, session) {
         M_pop <- bind_rows(M_pop)
 
             output$plot1 <- renderPlotly({
-                #set axis attributes
-                ax_att <-  list(title="", 
-                                ticks ="",
-                                showticklabels=FALSE,
-                                zeroline =FALSE, 
-                                showline =FALSE,
-                                showgrid=FALSE, 
-                                showspikes = FALSE)
-                #plot using heatmap
-                fig1 <- M_all_df %>% 
-                    plot_ly(x =~X, y=~Y, z = ~Z, frame = ~Iteration, type = "heatmap", showscale = FALSE) %>%
-                    layout(showlegend = FALSE, xaxis =ax_att, yaxis=ax_att, paper_bgcolor = "dimgray") %>%
-                    style(hoverinfo = 'none') %>% onRender("function(el,x) {Plotly.animate(el);}") %>% animation_slider(
-                        currentvalue = list(prefix = "Iteration ", color = "white"), font = list(color="white")) %>% animation_button(bgcolor = "black", font = list(color="white"))
-                
-                fig2 <- M_pop %>% plot_ly(x=~Iteration, y=~Rel_pop,frame = ~frame, type = 'scatter', mode = 'lines', line = list(simplyfy = F))  %>%
-                    layout(title = "Relative Population / %", yaxis = list(title = "Relative Population / %"), xaxis = list(title = "Life Cycle Iteration")) %>%
-                    onRender("function(el,x) {Plotly.animate(el);}") 
-                fig <- subplot(fig1, fig2) %>% 
-                    layout(title = 'Side By Side Subplots') %>% animation_slider(
-                        currentvalue = list(prefix = "Iteration ", color = "white"), font = list(color="white"))
-                fig
-        
+            #set axis attributes
+                    ax_att <-  list(title="", 
+                                    ticks ="",
+                                    showticklabels=FALSE,
+                                    zeroline =FALSE, 
+                                    showline =FALSE,
+                                    showgrid=FALSE, 
+                                    showspikes = FALSE)
+                    #plot using heatmap
+                    fig1 <- M_all_df %>% 
+                            plot_ly(
+                                x =~X, 
+                                y=~Y, 
+                                z = ~Z, 
+                                frame = ~Iteration, 
+                                type = "heatmap", 
+                                showscale = FALSE) %>%
+                            layout(
+                                showlegend = FALSE, 
+                                xaxis =ax_att, 
+                                yaxis=ax_att, 
+                                plot_bgcolor = "gray", 
+                                paper_bgcolor = "gray") %>%
+                    style(hoverinfo = 'none') %>% 
+                    onRender("function(el,x) {Plotly.animate(el);}") %>% 
+                    animation_slider(
+                                currentvalue = list(
+                                                    prefix = "Iteration ", 
+                                                    color = "white"), 
+                                font = list(
+                                    color="white"), 
+                                    tickcolor ="white") %>% 
+                        animation_button(
+                            bgcolor = "black", 
+                            font = list(color="white"))
             })
             output$plot2 <- renderPlotly({
-                fig2 <- M_pop %>% plot_ly(x=~Iteration, y=~Rel_pop,frame = ~frame, type = 'scatter', mode = 'lines', line = list(simplyfy = F))  %>%
-                    layout(title = "Relative Population / %", yaxis = list(title = "Relative Population / %"), xaxis = list(title = "Life Cycle Iteration")) %>%
-                    onRender("function(el,x) {Plotly.animate(el);}") %>% animation_opts(redraw = TRUE)
+                    fig2 <- M_pop %>% 
+                            plot_ly(
+                                    x=~Iteration, 
+                                    y=~Rel_pop,
+                                    frame = ~frame, 
+                                    type = 'scatter', 
+                                    mode = 'lines', 
+                                    line = list(
+                                        simplyfy = TRUE, 
+                                        color="white"))  %>%
+                            layout(
+                                paper_bgcolor = "gray", 
+                                plot_bgcolor = "gray", 
+                                yaxis = list(
+                                        title = list(
+                                                text ="Relative Population / %",
+                                                font= list(color ="white")), 
+                                        tickfont = list(
+                                                color = "white"),
+                                        linecolor = "white",
+                                        tickcolor = "white",
+                                        gridcolor = "white"), 
+                                xaxis = list(
+                                        title = list(
+                                                text ="Life Cycle Iteration", 
+                                                font= list(color="white")), 
+                                        tickfont = list(
+                                                color = "white"),
+                                        linecolor = "white",
+                                        tickcolor = "white",
+                                        gridcolor = "white",
+                                        range = c(0,frame_tot))) %>%
+                            onRender("function(el,x) {Plotly.animate(el);}") %>%
+                        animation_slider(
+                            currentvalue = list(
+                                prefix = "Iteration ", 
+                                color = "white"), 
+                            font = list(
+                                color="white"), 
+                            tickcolor ="white") %>% 
+                        animation_button(
+                            bgcolor = "black", 
+                            font = list(color="white"))
+                    
             })
     })
 
--- Game_of_Life/ui.R
+++ Game_of_Life/ui.R
@@ -16,22 +16,18 @@ library(plotly)
 library(htmlwidgets)
 
 
-# Define UI for application that draws a histogram
 fluidPage(
     setBackgroundColor(
         color = "dimgray",
         shinydashboard = FALSE
     ),
      # Application title
-    titlePanel(h1("Conway's Game of Life!",
+    titlePanel(h1("Conway's Game of Life", align ="center",
                   style= "color:white")),
     
 
     # Sidebar with a slider input for number of bins
-    fluidRow(
-        column(4,
-               wellPanel(style = "background: gray; color:white",
-                         
+    fluidRow(column(3, wellPanel(style = "background: gray; color:white",
                            sliderInput("habitat_size",
                                        "Habitat Size",
                                        min = 10,
@@ -40,18 +36,25 @@ fluidPage(
                            sliderInput("iterations",
                                        "Number of Generations",
                                        min = 1,
-                                       max = 150,
+                                       max = 100,
                                        value = 50),
                            sliderInput("prob_life",
                                        "Probability of starting life",
                                        min = 0.05,
                                        max = 0.5,
-                                       value = 0.15),
+                                       value = 0.15))),
+            column(3, wellPanel(style = "background: gray; color:white",
+                            numericInput("B_R", "Rebirth by reproduction if neighbours are exactly equal to:", value = 3, min = 0, max = 8),
+                            numericInput("D_U", "Death by under population if neighbours are less than:", value = 2, min = 0, max = 8),
+                            numericInput("D_O", "Death by overpopulation if neighbours are greater than:", value = 3, min = 0, max = 8),
                            actionButton("run", "Start", icon = icon("bolt"), 
-                                        style="background: black; color: white"),
-                        )
-               ),
-            ),
-    fluidRow(column(4, plotlyOutput("plot1")), column(4, plotlyOutput("plot2"))),
+                                        style="background: black; color: white")
+                        ))),
+            
+    fluidRow(column(4, wellPanel(style = "background: gray; color:white",
+                                 plotlyOutput("plot1"))), 
+             column(4, wellPanel(style = "background: gray; color:white",
+                                 plotlyOutput("plot2"))))
+   
     
 )
