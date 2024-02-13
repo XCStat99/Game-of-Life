b76fa6f70e50ea07d548c85ac8564561d9c20bfa
XCStat99 <alex.mcintosh1000@gmail.com>
2024-02-13 09:46:59 +0000

    Final

--- Game_of_Life/server.R
+++ Game_of_Life/server.R
@@ -1,22 +1,6 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
-library(shiny)
-library(gganimate)
-library(reshape2)
-library(tidyverse)
-library(plotly)
-library(htmlwidgets)
-
-
 function(input, output, session) {
-
+    #use eventReactive so that computation is only undertaken when the user presses run
+    #this prevents an wanted build up of tasks if the user plays with the sliders
     n <- eventReactive(input$run, {
      input$habitat_size 
       })
@@ -36,27 +20,34 @@ function(input, output, session) {
         input$D_O
     })
     observe({
-        #reassign variables to save on typing
-        #define 'habitat' size
-        #number of rows given by n
+        # a pop notification is created to warn the user that the system is
+        # working on the calculations
+        if(input$run > 0) {
+          showNotification("Please Wait...", type = "message")
+       }
+
+        # reassign variables to save on typing
+        # define 'habitat' size
+        # number of rows given by n
         n <- n()
-        #number of columns is a fixed ratio of the rows, this makes plotting easier
+        # number of columns is a arbitary fixed ratio of the rows, this makes plotting easier
         m <- as.integer(1.25*n())
-        #define evolution iterations
+        # define evolution iterations
         frame_tot <- frame_tot()
-        #initate starting conditions in a matrix, 
+        # initiate starting conditions in a matrix, 
         M<-matrix(rbinom(n*m,1,prob_life()),nrow=n)
         
-        #Alive and dead cells are assigned 1 or 0:
-        #Alive = 1
-        #Dead = 0
+        # alive and dead cells are assigned 1 or 0:
+        # alive = 1
+        # dead = 0
         
-        #create 3D matrix to store animation frames
+        # create 3D matrix to store animation frames
         M_all<-array(M, dim = c(dim(M),frame_tot))
-        #create data frame to store population stats
-        M_pop <- data.frame(Rel_pop = double(), Iteration = integer(),Colour = character())
-        #calculate initial population
+        # create data frame to store population stats
+        M_pop <- data.frame(Rel_pop = double(), Iteration = integer())
+        # calculate initial population
         M_pop_t0 <- sum(M)
+        
         for (i in 1:frame_tot){
             # make shifted copies of the original M matrix
             M1 = cbind( rep(0,n) , M[,-m] )
@@ -70,34 +61,27 @@ function(input, output, session) {
             
             # summation of the shifted matrices
             M_tot <- M1 + M2 + M3 + M4 + M5 + M6 + M7 + M8
-            
-            # apply the rules of Conway's Game of Life
+            #copy the matrix
             M_temp <- M
-            
-            
-            # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-            M_temp[M==0 & M_tot==B_R()] <- 1
-            # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+            # apply the rules of Conway's Game of Life:
+            # any live cell with fewer than two live neighbours dies, as if by under population.
             M_temp[M==1 & M_tot<D_U()] <- 0
-            # Any live cell with more than three live neighbours dies, as if by overpopulation.
+            # any live cell with more than three live neighbours dies, as if by overpopulation.
             M_temp[M==1 & M_tot>D_O()] <- 0
-            #populate 3D matrix for animation
+            # any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            M_temp[M==0 & M_tot==B_R()] <- 1
+            # populate 3D matrix for animation
             M_all[,,i] <- M_temp
-            
-            #if (i==1){
-            #    M_pop[nrow(M_pop)+1,] <- c(0,i,"white")
-            #}
-            #check relative population to starting population and store to vector
-            rel_pop <- (sum(M_temp) - M_pop_t0)*100/M_pop_t0
-            if (sum(M_temp) > sum(M)){
-                col_change <- "blue"
-            }else{
-                col_change <- "red"
+            # here the starting value for the population change is set as zero to allow for nicer plotting
+            if (i==1){
+                M_pop[nrow(M_pop)+1,] <- c(0,1)
             }
-            M_pop[nrow(M_pop)+1,] <- c(rel_pop,i,0)
+            # check relative population to starting population and store to vector
+            rel_pop <- (sum(M_temp) - M_pop_t0)*100/M_pop_t0
+            M_pop[nrow(M_pop)+1,] <- c(rel_pop,i+1)
             M <- M_temp
-            #check if mass extinction has occured
-            if(rel_pop ==0){
+            # check if mass extinction has occurred
+            if(sum(M_temp) ==0){
                 M_all <- M_all[,,1:i]
                 break
             }
@@ -106,97 +90,101 @@ function(input, output, session) {
         }
         
         
-        #melt 3D matrix to dataframe to allow plotting
+        # melt 3D matrix to a dataframe to allow plotting with plotly
         M_all_df <-  melt(M_all)
         colnames(M_all_df) <- c('X', 'Y', 'Iteration', 'Z')
-        #accumulate data to allow trace scatter to be created
+        # accumulate data to allow trace line graph of relative population changes to be created
         M_pop <- lapply(seq_along(M_pop$Iteration), function(x) {
             cbind(M_pop[M_pop$Iteration %in% M_pop$Iteration[seq(1, x)], ], frame = M_pop$Iteration[[x]])})
         M_pop <- bind_rows(M_pop)
-
-            output$plot1 <- renderPlotly({
-            #set axis attributes
-                    ax_att <-  list(title="", 
-                                    ticks ="",
-                                    showticklabels=FALSE,
-                                    zeroline =FALSE, 
-                                    showline =FALSE,
-                                    showgrid=FALSE, 
-                                    showspikes = FALSE)
-                    #plot using heatmap
-                    fig1 <- M_all_df %>% 
-                            plot_ly(
-                                x =~X, 
-                                y=~Y, 
-                                z = ~Z, 
-                                frame = ~Iteration, 
-                                type = "heatmap", 
-                                showscale = FALSE) %>%
-                            layout(
-                                showlegend = FALSE, 
-                                xaxis =ax_att, 
-                                yaxis=ax_att, 
-                                plot_bgcolor = "gray", 
-                                paper_bgcolor = "gray") %>%
-                    style(hoverinfo = 'none') %>% 
-                    onRender("function(el,x) {Plotly.animate(el);}") %>% 
-                    animation_slider(
-                                currentvalue = list(
-                                                    prefix = "Iteration ", 
-                                                    color = "white"), 
-                                font = list(
-                                    color="white"), 
-                                    tickcolor ="white") %>% 
-                        animation_button(
-                            bgcolor = "black", 
-                            font = list(color="white"))
-            })
-            output$plot2 <- renderPlotly({
-                    fig2 <- M_pop %>% 
-                            plot_ly(
-                                    x=~Iteration, 
-                                    y=~Rel_pop,
-                                    frame = ~frame, 
-                                    type = 'scatter', 
-                                    mode = 'lines', 
-                                    line = list(
-                                        simplyfy = TRUE, 
-                                        color= ~Colour))  %>%
-                            layout(
-                                paper_bgcolor = "gray", 
-                                plot_bgcolor = "gray", 
-                                yaxis = list(
-                                        title = list(
-                                                text ="Relative Population Change/ %",
-                                                font= list(color ="white")), 
-                                        tickfont = list(
-                                                color = "white"),
-                                        linecolor = "white",
-                                        tickcolor = "white",
-                                        gridcolor = "white"), 
-                                xaxis = list(
-                                        title = list(
-                                                text ="Life Cycle Iteration", 
-                                                font= list(color="white")), 
-                                        tickfont = list(
-                                                color = "white"),
-                                        linecolor = "white",
-                                        tickcolor = "white",
-                                        gridcolor = "white",
-                                        range = c(0,frame_tot))) %>%
-                            onRender("function(el,x) {Plotly.animate(el);}") %>%
-                        animation_slider(
-                            currentvalue = list(
-                                prefix = "Iteration ", 
-                                color = "white"), 
-                            font = list(
-                                color="white"), 
-                            tickcolor ="white") %>% 
-                        animation_button(
-                            bgcolor = "black", 
-                            font = list(color="white"))
-                    
-            })
+        # here the relative population changes are plotted to plot2 in the ui
+        output$plot1 <- renderPlotly({
+                            #set axis attributes
+                            ax_att <-  list(title="", 
+                                            ticks ="",
+                                            showticklabels=FALSE,
+                                            zeroline =FALSE, 
+                                            showline =FALSE,
+                                            showgrid=FALSE, 
+                                            showspikes = FALSE)
+                            #plot using heatmap
+                            fig1 <- M_all_df %>% 
+                                    plot_ly(
+                                        x =~X, 
+                                        y=~Y, 
+                                        z = ~Z, 
+                                        frame = ~Iteration, 
+                                        type = "heatmap", 
+                                        showscale = FALSE) %>%
+                                    layout(
+                                        showlegend = FALSE, 
+                                        xaxis =ax_att, 
+                                        yaxis=ax_att, 
+                                        plot_bgcolor = "gray", 
+                                        paper_bgcolor = "gray") %>%
+                                    style(hoverinfo = 'none') %>% 
+                                    # onRender added to start the animation as soon as the calculations are complete
+                                    onRender("function(el,x) {Plotly.animate(el);}") %>% 
+                                    animation_slider(
+                                    currentvalue = list(
+                                                        prefix = "Iteration ", 
+                                                        color = "white"), 
+                                                        font = list(
+                                                            color="white"), 
+                                                            tickcolor ="white") %>% 
+                                    animation_button(
+                                        bgcolor = "black", 
+                                        font = list(color="white"))
+                        })
+        # here the relative population changes are plotted to plot2 in the ui
+        output$plot2 <- renderPlotly({
+                            fig2 <- M_pop %>% 
+                                    plot_ly(
+                                            x=~Iteration, 
+                                            y=~Rel_pop,
+                                            frame = ~frame, 
+                                            type = 'scatter', 
+                                            mode = 'lines', 
+                                            line = list(
+                                                simplyfy = TRUE,
+                                                color="yellow"))  %>%
+                                    layout(
+                                        paper_bgcolor = "gray", 
+                                        plot_bgcolor = "gray", 
+                                        yaxis = list(
+                                                title = list(
+                                                        text ="Relative Population Change/ %",
+                                                        font= list(color ="white")), 
+                                                tickfont = list(
+                                                        color = "white"),
+                                                linecolor = "white",
+                                                tickcolor = "white",
+                                                gridcolor = "white"), 
+                                        xaxis = list(
+                                                title = list(
+                                                        text ="Life Cycle Iteration", 
+                                                        font= list(color="white")), 
+                                                tickfont = list(
+                                                        color = "white"),
+                                                linecolor = "white",
+                                                tickcolor = "white",
+                                                gridcolor = "white",
+                                                range = c(0,frame_tot))) %>%
+                                    # onRender added to start the animation as soon as the calculations are complete
+                                    onRender("function(el,x) {Plotly.animate(el);}") %>%
+                                    animation_slider(
+                                        currentvalue = list(
+                                            prefix = "Iteration ", 
+                                            color = "white"), 
+                                        font = list(
+                                            color="white"), 
+                                        tickcolor ="white") %>% 
+                                    animation_button(
+                                        bgcolor = "black", 
+                                        font = list(color="white"))
+                
+                        })
+            
     })
 
 }
--- Game_of_Life/ui.R
+++ Game_of_Life/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(shinyWidgets)
 library(gganimate)
@@ -15,50 +6,74 @@ library(tidyverse)
 library(plotly)
 library(htmlwidgets)
 
-
 fluidPage(
+   
     setBackgroundColor(
         color = "dimgray",
         shinydashboard = FALSE
     ),
-     # Application title
-    titlePanel(h1("Conway's Game of Life", align ="center",
-                  style= "color:white")),
-    
+     # application title and formatting 
+    titlePanel(windowTitle = "Conway's Game of Life", 
+               h1(strong("Conway's Game of Life"), align ="center",
+                  style= "color:white")
+               ),
 
-    # Sidebar with a slider input for number of bins
+    # set input sliders and numeric inputs into a separate well panel 
     fluidRow(column(2, wellPanel(style = "background: gray; color:white",
+                            # create slider for defining the size of the game
                            sliderInput("habitat_size",
-                                       "Habitat Size",
+                                       "Habitat size",
                                        min = 10,
-                                       max = 100,
+                                       max = 80,
                                        value = 50),
+                            # create slider for defining the number of generations
                            sliderInput("iterations",
-                                       "Number of Generations",
-                                       min = 1,
+                                       "Number of generations",
+                                       min = 2,
                                        max = 100,
                                        value = 50),
+                           # create slider for defining probability of starting life
+                           # this is the p value in the binomial distribution used to 
+                           # create the starting state
                            sliderInput("prob_life",
                                        "Probability of starting life",
                                        min = 0.05,
                                        max = 0.5,
                                        value = 0.15),
-                           numericInput("B_R", "Rebirth by reproduction if neighbours are exactly equal to:", value = 3, min = 0, max = 8),
-                            numericInput("D_U", "Death by under population if neighbours are less than:", value = 2, min = 0, max = 8),
-                            numericInput("D_O", "Death by overpopulation if neighbours are greater than:", value = 3, min = 0, max = 8),
+                           # create number inputs for changing the rules of the game of life
+                           numericInput("D_U", "Death by underpopulation if neighbours are less than:", 
+                                        value = 2, min = 0, max = 8),
+                           numericInput("D_O", "Death by overpopulation if neighbours are greater than:", 
+                                        value = 3, min = 0, max = 8),
+                           numericInput("B_R", "Rebirth by reproduction if neighbours are exactly equal to:", 
+                                        value = 3, min = 0, max = 8),
                            actionButton("run", "Start", icon = icon("bolt"), 
-                                        style="background: black; color: white"))
-                        ),
-            
-    column(6, wellPanel(style = "background: gray; color:white",
-                        div(style="width:100%;height:0;padding-top:100%;position:relative;",
-                            div(style="position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;",plotlyOutput("plot1", height ="100%"))))), 
+                                        style="background: black; color: white; width: 100%"))
+            ),
+            # create a wellpanel and plot the heatmap which shows the game's life patterns
+            # evolving. The wrapped up div calls with dimensions are to allow the heatmap 
+            # to be plotted square and centrally in the wellpanel
+            column(6, wellPanel(style = "background: gray; color:white",
+                        div(style="width: 100%; height: 0; padding-top: 100%; position:relative;",
+                            div(style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;",
+                                plotlyOutput("plot1", height ="100%"))))), 
+            # create a second wellpanel to plot the relative population data
              column(4, wellPanel(style = "background: gray; color:white",
-                                 plotlyOutput("plot2")))
+                                 plotlyOutput("plot2")),
+                    # a well panel in the bottom right is created to give an introduction to the app
+                    # for the user and to link to the appropriate code on github and the README.
+                        wellPanel(style = "background: gray; color: white",
+                            p("This is an implementation of Conway’s Game of Life using R and Plotly. 
+                              This was an exercise in using R, Plotly and Shiny for implementing the Game of Life."),
+                            p("Once ‘Start’ is pressed all generations of the habitat are calculated prior to plotting, 
+                            this combined with the use of Plotly, does lead to some limitations with the performance 
+                            of the system. Most of the calculation time is used for generating the habitat plot. 
+                            However, this approach of creating the whole lifespan of the game and plotting with Plotly 
+                            does offer some nice features. For example, being able to interactively zoom in on certain areas 
+                            of the habitat, rewind and pause generations. Allowing the birth and death of different patterns to be observed. "),
+                            p("More details can be found ", a("here", href = "https://github.com/XCStat99/Game-of-Life"))
+                    )
+            )
    
-    )
+        )
 )
